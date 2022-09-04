376348463f73a219e7722da282431c497e8120b4
Yousof Wakili <ywakili18@gmail.com>
2022-09-04 14:55:24 -0700

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500&display=swap"
             rel="stylesheet"
           />
