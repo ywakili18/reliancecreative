efb29f38ca6f0d02af8882a4b94d988917faccf3
Yousof Wakili <ywakili18@gmail.com>
2022-09-06 22:48:55 -0700

    opacity

--- components/work-components/WorkImage.js
+++ components/work-components/WorkImage.js
@@ -5,8 +5,8 @@ const WorkImage = ({ alt, image }) => {
   return (
     <motion.div
       className="h-full flex flex-col"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
       <Image
